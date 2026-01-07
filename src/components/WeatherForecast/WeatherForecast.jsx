527a569991f3b3c2a9d716377635ba5b1752c0a7
“CindyS” <veggiebyte@yahoo.com>
2026-01-07 15:50:47 -0800

    Base lesson done

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,15 @@
+// src/components/WeatherForecast/WeatherForecast.jsx
+import './WeatherForecast.css';
+
+const WeatherForecast = (props) => {
+  return (
+    <div className="weather">
+      <h2>{props.day}</h2>
+      <img src={props.img} alt={props.imgAlt} />
+      <p><span>conditions: </span>{props.conditions}</p>
+      <p><span>time: </span>{props.time}</p>
+    </div>
+  );
+}
+
+export default WeatherForecast;
